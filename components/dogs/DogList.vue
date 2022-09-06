7df7fafcb71d1b10620d7296aa86be3c54be0db7
Will Aronow <waronow123@gmail.com>
2022-09-05 18:05:47 -0700

    created dog list component

---
+++ components/dogs/DogList.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <v-row>
+    <v-col xl="2" md="3" sm="6" v-for="dog in dogs" :key="dog.id">
+      <DogPreview :dog="dog" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import DogPreview from '~/components/dogs/DogPreview.vue'
+export default {
+  components: {
+    DogPreview
+  },
+  data() {
+    return {
+      dogs: [
+        { id: '1', name: 'Bebe', dob: '01/20/2003', sex: 'F', description: 'Fun loving girl who enjoys treats and love', location: 'Tucson, AZ', parents: 'Hoover & Tiggy', color: 'black and white' },
+        { id: '2', name: 'Knuckles', dob: '05/20/2010', sex: 'M', description: 'Big goof ball who likes to play and enjoys laying in flowers', location: 'Nogales, AZ', parents: 'Unknown', color: 'black and white' },
+        { id: '3', name: 'Xena', dob: '04/20/2022', sex: 'F', description: 'Fiesty little princess who is #1 and knows it', location: 'Casa Grand, AZ', parents: 'Unknown', color: 'Blue and White' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,21 @@
 <template>
   <section>
-    <TheHero />
-    <h2 class="my-5 text-center">Current Puppies Available</h2>
-    <v-row>
-      <v-col cols="3">
-        <DogPreview />
-      </v-col>
-      <v-col cols="3">
-        <DogPreview />
-      </v-col>
-      <v-col cols="3">
-        <DogPreview />
-      </v-col>
-      <v-col cols="3">
-        <DogPreview />
-      </v-col>
-    </v-row>
+    <article class="">
+      <TheHero />
+    </article>
+    <article class="">
+      <h2 class="my-5 text-center">Current Puppies Available</h2>
+      <DogList />
+    </article>
   </section>
 </template>
 
 <script>
-import DogPreview from '~/components/dogs/DogPreview.vue'
+import DogList from '~/components/dogs/DogList.vue'
 export default {
   name: 'IndexPage',
   components: {
-    DogPreview
+    DogList
   }
 }
 </script>
